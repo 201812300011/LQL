78a515cc7f9978ba4c26f682b38e0b3fd0844244
201812300011 <2505642660@qq.com>
2020-05-30 07:41:18 +0800

    基于对话框，一运行，图片直接显示在对话框上
    Signed-off-by: 201812300011 <2505642660@qq.com>

---
+++ 第13周综合实验2.2/第13周综合实验2.2/第13周综合实验2.2.h
@@ -0,0 +1,32 @@
+
+// 第13周综合实验2.2.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C第13周综合实验22App: 
+// 有关此类的实现，请参阅 第13周综合实验2.2.cpp
+//
+
+class C第13周综合实验22App : public CWinApp
+{
+public:
+	C第13周综合实验22App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C第13周综合实验22App theApp;
